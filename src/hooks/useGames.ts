624c7d173c5273e336c8e524d5e18038f87a0aec
SHUNG245 <1291149034@qq.com>
2024-04-24 17:52:28 +0800

    feat : display icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
-interface Game {
+export interface Platform{
+    id:number;
+    name:string;
+    slug:string
+}
+export interface Game {
     id: number;
     name: string;
+    background_image:string;
+    parent_platforms:{platform:Platform}[]
   }
-  interface FetchGamesResponse {
+interface FetchGamesResponse {
     count: number;
     results: Game[];
   }
